eb5762d74fdcff825a056bb823073fb3e151ff81
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2024-03-20 11:55:48 -0300

    Actualizar Lab_Codigo_1.java

--- Unidad 1/Clase 1/Lab_Codigo_1.java
+++ Unidad 1/Clase 1/Lab_Codigo_1.java
@@ -1,5 +1,28 @@
-package Unidad 1.Clase 1;
+public class Multiplicacion {
 
-public class Lab_Codigo_1 {
-    
+    // Función no recursiva
+    public static int multiplicacionNoRecursiva(int a, int b) {
+        int resultado = 0;
+        for (int i = 0; i < b; i++) {
+            resultado += a;
+        }
+        return resultado;
+    }
+
+    // Función recursiva
+    public static int multiplicacionRecursiva(int a, int b) {
+        if (b == 0) {
+            return 0;
+        } else if (b < 0) {
+            return -multiplicacionRecursiva(a, -b);
+        } else {
+            return a + multiplicacionRecursiva(a, b - 1);
+        }
+    }
+
+    // Ejemplo de uso
+    public static void main(String[] args) {
+        System.out.println("Multiplicación no recursiva: " + multiplicacionNoRecursiva(5, 3));
+        System.out.println("Multiplicación recursiva: " + multiplicacionRecursiva(5, 3));
+    }
 }
