df16b2a2c374df3cd27ee66c022adac888208008
Ignacio Rehbein <Ignacioalfredorm@gmail.com>
2024-04-10 12:14:45 -0400

    update

--- Solemne 1/Main1.java
+++ Solemne 1/Main1.java
@@ -2,31 +2,24 @@ import java.util.Scanner;
 
 public class Main1 {
   public static void main(String[] args) {
+
     Scanner scanner = new Scanner(System.in);
     System.out.println("Ingrese la cantidad de números del vector: ");
     int n = scanner.nextInt();
-    int[] array = new int[n]; // Inicialización del array
 
+    int[] array = new int[n]; // Inicialización el array
+
+    // Ingreso de los números
     for (int i = 0; i < n; i++) {
       System.out.println("Ingrese el número " + (i + 1) + ": ");
-      array[i] = scanner.nextInt(); // Población del array
+      array[i] = scanner.nextInt(); // Ingreso de los números
     }
-
-    System.out.println("Suma no recursiva: " + sumNonRecursive(array));
     System.out.println("Suma recursiva: " + sumRecursive(array, 0));
-  }
-
-  // Método no recursivo para sumar elementos
-  public static int sumNonRecursive(int[] array) {
-    int sum = 0;
-    for (int num : array) {
-      sum += num;
-    }
-    return sum;
   }
 
   // Método recursivo para sumar elementos
   public static int sumRecursive(int[] array, int index) {
+
     if (index == array.length) {
       return 0;
     } else {
